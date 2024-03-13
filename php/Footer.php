e5999d595022a558d6610b67fe123b42bf65a0a1
joaobragheroli <joao.bragheroli@gmail.com>
2024-03-13 16:20:39 -0300

    Novidade

--- php/Footer.php
+++ php/Footer.php
@@ -1,7 +1,7 @@
 <head>
-    <link rel="stylesheet" href="../css/Footer.css">
+    <!-- <link rel="stylesheet" href="../css/Footer.css"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <script src="https://unpkg.com/@phosphor-icons/web"></script>
+    <script defer src="https://unpkg.com/@phosphor-icons/web"></script>
 </head>
 <footer>
         <div class="conteiner">
