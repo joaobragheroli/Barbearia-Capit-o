0f07b2d4f207c37bfe8e53088cba8d6ae5338b59
mlkluan <luanders84@gmail.com>
2024-04-23 15:44:42 -0300

    login css

--- php/login.php
+++ php/login.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" href="../img/img.ico" type="image/x-icon">
-        <link rel="stylesheet" href="./css/login.css">
+        <link rel="stylesheet" href="../css/login.css">
         <title>Login</title>
     </head>
 
