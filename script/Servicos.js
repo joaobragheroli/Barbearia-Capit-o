06929506329743de88debaf5b6210adf3f1f70ba
raquelAlmeidaa <rochellesilva5513@gmail.com>
2024-09-27 16:17:37 -0300

    novas atualizações

--- script/Servicos.js
+++ script/Servicos.js
@@ -31,3 +31,29 @@ const swiper = new Swiper('.slider-wrapper', {
     }
 
 });
+
+// Faz chegar a frase no "Home-IMG" e outros elementos
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+// Seleciona todos os elementos com a classe 'hidden' e o elemento 'sectionIMG'
+const hiddenElements = document.querySelectorAll('.hidden');
+
+
+// Observa todos os elementos com a classe 'hidden'
+hiddenElements.forEach((element) => myObserver.observe(element));
+
+
+// Função para atualizar as iniciais no header
+function updateUserInitials(firstName, lastName) {
+    const initials = (firstName.charAt(0) + lastName.charAt(0)).toUpperCase();
+    document.getElementById('initials').textContent = initials;
+    document.getElementById('user-initials').style.display = 'block';
+}
