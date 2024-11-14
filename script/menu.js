be18fc076d47b12ffb5f4923052c928c7fe47553
joaobragheroli <joao.bragheroli@gmail.com>
2024-11-14 16:19:14 -0300

    coisa nova

--- script/menu.js
+++ script/menu.js
@@ -2,8 +2,8 @@ const modal = document.querySelector(".modalgeral");
 var btnAbrir = document.getElementById("mostrarModal");
 var btnFechar = document.getElementById("fecharModal");
 
-// btnAbrir.addEventListener("click", exibirModal);
-// btnFechar.addEventListener("click", fecharModal);
+btnAbrir.addEventListener("click", exibirModal);
+btnFechar.addEventListener("click", fecharModal);
 
 function exibirModal() {
     modal.classList.toggle("aberto");
